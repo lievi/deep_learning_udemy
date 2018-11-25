2c8999f3689a53a14eda1506a781d94d30569b2c
lievi <lievi.silva@gmail.com>
2018-11-25 16:43:01 -0200

    Adição dos layers do ANN

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv(
+    "Deep_Learning_A_Z\Volume 1 - Supervised Deep Learning\Part 1 - Artificial Neural Networks (ANN)\Section 4 - Building an ANN\Churn_Modelling.csv"
+)
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
@@ -20,7 +22,7 @@ onehotencoder = OneHotEncoder(categorical_features=[1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
+# Splitting the dataset into the Training set and Test set'
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=0)
@@ -34,9 +36,30 @@ X_test = sc.transform(X_test)
 # Fitting classifier to the Training set
 # Create your classifier here
 
+# Importing Keras
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(
+    Dense(
+        units=6, kernel_initializer='uniform', activation='relu',
+        input_dim=11))
+
+# Adding the second hidden layer
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+
+# Adding the output layer
+classifier.add(
+    Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)
+# y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+# from sklearn.metrics import confusion_matrix
+# cm = confusion_matrix(y_test, y_pred)
